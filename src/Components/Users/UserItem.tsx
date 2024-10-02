3384888bcb2245847e9ee4ce6d1c399ea8828b2f
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-02 14:47:06 +0200

    Adjusted a few styles and homework is done

--- src/Components/Users/UserItem.tsx
+++ src/Components/Users/UserItem.tsx
@@ -9,10 +9,10 @@ const UserItem: React.FC<Props> = ({user}) => {
   return (
     <div className="card mb-3 p-4">
         <div className="col-8">
-          <h5 className="card-title">{user.name}</h5>
-          <p className="card-text">Email: {user.email}</p>
-          <p className="card-text">Active: {user.active ? 'Yes' : 'No'}</p>
-          <p className="card-text">Role: {user.role}</p>
+          <h5 className="card-title">User name: {user.name}</h5>
+          <p className="card-text">Email: <strong>{user.email}</strong></p>
+          <p className="card-text">Active: <strong>{user.active ? 'Yes' : 'No'}</strong></p>
+          <p className="card-text">Role: <strong>{user.role}</strong></p>
         </div>
     </div>
   );
